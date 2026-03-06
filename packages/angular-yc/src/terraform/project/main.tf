6d93826db7702c50dcd2054d7529b6b63720960e
Dmitry Sercov <serkov.dimitri@yandex.ru>
2026-03-06 20:09:52 +0300

    fix: replace empty env var values with placeholders (#45)
    Yandex Cloud Functions rejects empty string environment variable
    values. Use "0" for AYC_DEBUG and "disabled" for YDB vars when
    their features are off.

--- packages/angular-yc/src/terraform/project/main.tf
+++ packages/angular-yc/src/terraform/project/main.tf
@@ -209,11 +209,11 @@ resource "yandex_function" "server" {
       NODE_ENV              = "production"
       ASSETS_BUCKET         = local.assets_bucket
       NG_ALLOWED_HOSTS      = var.domain_name
-      AYC_DEBUG             = var.env != "production" ? "1" : ""
-      YDB_ENDPOINT          = var.enable_response_cache ? yandex_ydb_database_serverless.response_cache[0].ydb_full_endpoint : ""
-      YDB_DATABASE          = var.enable_response_cache ? yandex_ydb_database_serverless.response_cache[0].database_path : ""
-      YDB_ACCESS_KEY_ID     = var.enable_response_cache ? yandex_iam_service_account_static_access_key.ydb[0].access_key : ""
-      YDB_SECRET_KEY        = var.enable_response_cache ? yandex_iam_service_account_static_access_key.ydb[0].secret_key : ""
+      AYC_DEBUG             = var.env != "production" ? "1" : "0"
+      YDB_ENDPOINT          = var.enable_response_cache ? yandex_ydb_database_serverless.response_cache[0].ydb_full_endpoint : "disabled"
+      YDB_DATABASE          = var.enable_response_cache ? yandex_ydb_database_serverless.response_cache[0].database_path : "disabled"
+      YDB_ACCESS_KEY_ID     = var.enable_response_cache ? yandex_iam_service_account_static_access_key.ydb[0].access_key : "disabled"
+      YDB_SECRET_KEY        = var.enable_response_cache ? yandex_iam_service_account_static_access_key.ydb[0].secret_key : "disabled"
       REVALIDATE_SECRET_ID  = module.security.revalidate_secret_id
       CACHE_PURGE_SECRET_ID = module.security.revalidate_secret_id
     }
